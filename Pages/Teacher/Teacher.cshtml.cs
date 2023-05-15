d977df0a36c2acf4c435b46b2aab4407990337b7
Camilo1914z <camilo1914z@gmail.com>
2023-05-15 15:48:08 -0500

    Autenticacion-Logout

--- Pages/Teacher/Teacher.cshtml.cs
+++ Pages/Teacher/Teacher.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,7 @@ namespace Autenticacion.Pages.Teacher
 {
     public class TeacherModel : PageModel
     {
+        [Authorize]
         public void OnGet()
         {
         }
